1eebb3c2b0ce2892862455534e4ce207171a603b
vladimir <balakirev.vladimir@gmail.com>
2018-03-16 16:04:26 +0300

    Publisher container (#7)
    * docker container to make db image with data
    
    * working container
    
    * removed waiting of db start
    
    * fixes from comments: check if dump exists, remove env values

---
+++ publisher/start.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+docker login -p=$DOCKERHUB_PASS -u=$DOCKERHUB_LOGIN
+docker pull plyo/postgres:9.5.10-1.0.1
+
+docker run --name plyo_postgres -d -e POSTGRES_PASS -e PLYO_PASS -e APP_PASS \
+-v $PUBLISHER_DIR/restore.sh:/restore.sh \
+-v $DUMPS_DIR:/files \
+plyo/postgres:9.5.10-1.0.1
+
+docker exec plyo_postgres mkdir /dumps
+backup_date=`date +%Y-%m-%d-%H_00`
+backup_file="${backup_date}"-preview-db-5432-hourly.backup
+
+if [ $(docker exec plyo_postgres test -e "/files/$backup_file" && echo $?) ];
+then
+    docker exec plyo_postgres cp /files/${backup_file} /dumps/plyo.backup
+#    restore.sh will be executed at the moment of container's start
+    docker exec plyo_postgres cp /restore.sh /docker-entrypoint-initdb.d/restore.sh
+
+    docker commit $(docker ps -f name=plyo_postgres -q) plyo/postgres:publisher
+    docker push plyo/postgres:publisher
+else
+    echo file "${backup_file}" is not found
+fi
+
+docker kill plyo_postgres
+docker rm plyo_postgres
