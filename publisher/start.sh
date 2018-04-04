46f615a58a46be3c2ddff605e3b4d16ac3ee8096
Andrew Balakirev <balakirev.andrey@gmail.com>
2018-04-04 13:54:24 +0300

    support for custom backup file name and postgres image

--- publisher/start.sh
+++ publisher/start.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
-docker login -p=$DOCKERHUB_PASS -u=$DOCKERHUB_LOGIN
-docker pull plyo/postgres:9.5.10-1.0.1
+docker login -p=${DOCKERHUB_PASS} -u=${DOCKERHUB_LOGIN}
+docker pull ${POSTGRES_IMAGE}
 
 docker run --name plyo_postgres -d -e POSTGRES_PASS -e PLYO_PASS -e APP_PASS \
--v $DUMPS_DIR:/files \
+-v ${DUMPS_DIR}:/files \
 -w /docker-entrypoint-initdb.d \
 plyo/postgres:9.5.10-1.0.1
 
 docker exec plyo_postgres mkdir /dumps
 backup_date=`date +%Y-%m-%d-%H:00`
-backup_file="${backup_date}"-preview-db-5432-hourly.backup
+backup_file=${backup_date}${DUMP_NAME_SUFFIX}
 
 if [ $(docker exec plyo_postgres test -e "/files/$backup_file" && echo $?) ];
 then
     docker exec plyo_postgres cp /files/${backup_file} /dumps/plyo.backup
-#    restore.sh will be executed at the moment of container's start
+
+    # restore.sh will be executed at the moment of container's start
     (docker exec -i plyo_postgres sh -c "cat > restore.sh") < restore.sh
 
     docker commit $(docker ps -f name=plyo_postgres -q) plyo/postgres:data
     docker push plyo/postgres:data
 else
-    echo file "${backup_file}" is not found
+    echo file ${backup_file} is not found
 fi
 
 docker kill plyo_postgres
