399d904a4977e578aa7aabd9fba2d418d9198a49
Max Ryndin <rgmasim@gmail.com>
2018-11-26 10:00:07 +0300

    PLYO-652 Sanitize emails table.

--- publisher/start.sh
+++ publisher/start.sh
@@ -34,8 +34,27 @@ EORESTORE
       pg_restore /dumps/db.backup -U postgres -d ${DB_NAME}
 EORESTORE
 
-    # TODO: sanitize DB here
+    # we need to sanitize sensitive data first
+    docker run --name sanitizing_db_container -d --rm -v ${DUMPS_DIR}:/files \
+    -e POSTGRES_DB=${DB_NAME} -e POSTGRES_PASSWORD= postgres:10.5
+    # wait for "database system is ready to accept connections", hoping 4 sec is enough
+    sleep 4;
 
+    docker exec sanitizing_db_container psql -f /files/${backup_roles_file} -U postgres
+    docker exec sanitizing_db_container pg_restore /files/${backup_file} -U postgres -d ${DB_NAME}
+
+    cat <<-EOSANITIZE | (docker exec -i sanitizing_db_container sh -c "cat > sanitize.sql")
+          UPDATE emails
+          SET
+            email = regexp_replace(email, '(^[^@]+)', md5(email)),
+            name  = md5(name);
+EOSANITIZE
+
+    docker exec sanitizing_db_container psql -f sanitize.sql -U postgres ${DB_NAME}
+    docker exec sanitizing_db_container pg_dump -Fc -U postgres -f /files/${backup_file}.sanitized ${DB_NAME} > /dev/null
+    docker stop sanitizing_db_container
+
+    docker exec publishing_db_container cp /files/${backup_file}.sanitized /dumps/db.backup
     docker commit $(docker ps -a -f name=publishing_db_container -q) ${DESTINATION_DOCKER_IMAGE}
     docker push ${DESTINATION_DOCKER_IMAGE}
 else
