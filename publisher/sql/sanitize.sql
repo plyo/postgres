58b1dec1055972b005fed716830f61e9565d02f6
Max Ryndin <rgmasim@gmail.com>
2018-11-27 00:23:13 +0300

    PLYO-652 Sanitise more data.

--- publisher/sql/sanitize.sql
+++ publisher/sql/sanitize.sql
@@ -1,4 +1,37 @@
 UPDATE emails
 SET
+  email            = regexp_replace(email, '(^[^@]+)', md5(email)),
+  "recipientEmail" = regexp_replace("recipientEmail", '(^[^@]+)', md5("recipientEmail")),
+  name             = md5(name);
+
+UPDATE emails
+SET
+  phone = 93000000
+WHERE phone IS NOT NULL;
+
+UPDATE users
+SET
+  email         = regexp_replace(email, '(^[^@]+)', md5(email)),
+  "displayName" = md5("displayName");
+
+UPDATE users
+SET
+  phone = 93000000
+WHERE phone IS NOT NULL;
+
+UPDATE contacts
+SET
+  phone = '93000000'
+WHERE phone IS NOT NULL;
+
+UPDATE contacts
+SET
+  name  = md5(name),
   email = regexp_replace(email, '(^[^@]+)', md5(email)),
-  name  = md5(name);
+  "fromEmail" = regexp_replace("fromEmail", '(^[^@]+)', md5("fromEmail"));
+
+UPDATE members
+SET
+  "firstName" = RIGHT(md5("firstName"), 20),
+  "lastName"  = RIGHT(md5("lastName"), 20),
+  email       = RIGHT(regexp_replace(email, '(^[^@]+)', md5(email)), 40);
