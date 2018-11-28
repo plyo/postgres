278ea529e11571eb3333a34471acc14bf0345546
Max Ryndin <rgmasim@gmail.com>
2018-11-28 23:17:25 +0300

    PLYO-652 Add comments.

--- publisher/sql/sanitize.sql
+++ publisher/sql/sanitize.sql
@@ -1,3 +1,8 @@
+
+-- We define sanitization functions first:
+-- - sanitize_email
+-- - sanitize_phone
+-- - sanitize_value
 CREATE OR REPLACE FUNCTION sanitize_email(_t VARCHAR, _c VARCHAR)
   RETURNS VOID AS
 $$
@@ -37,6 +42,18 @@ END
 $$
 LANGUAGE plpgsql;
 
+-- This query is for demo purposes only. It shows all the columns with non-null comments.
+-- The query is a base for filtering columns to sanitize.
+SELECT
+  c.table_name,
+  c.column_name,
+  pgd.description
+FROM pg_catalog.pg_statio_all_tables AS st
+  INNER JOIN pg_catalog.pg_description pgd ON (pgd.objoid = st.relid)
+  INNER JOIN information_schema.columns c ON (pgd.objsubid = c.ordinal_position
+                                              AND c.table_schema = st.schemaname AND c.table_name = st.relname);
+
+-- Call sanitize_email on every column containing SANITIZE_AS_EMAIL in the comment
 SELECT sanitize_email(res.table_name :: VARCHAR, res.column_name :: VARCHAR)
 FROM (SELECT
         c.table_name,
@@ -47,6 +64,7 @@ FROM (SELECT
                                                     AND c.table_schema = st.schemaname AND c.table_name = st.relname)
       WHERE pgd.description LIKE '%SANITIZE_AS_EMAIL%') AS res;
 
+-- Call sanitize_value on every column containing SANITIZE_AS_VALUE in the comment
 SELECT sanitize_value(res.table_name :: VARCHAR, res.column_name :: VARCHAR)
 FROM (SELECT
         c.table_name,
@@ -57,6 +75,7 @@ FROM (SELECT
                                                     AND c.table_schema = st.schemaname AND c.table_name = st.relname)
       WHERE pgd.description LIKE '%SANITIZE_AS_VALUE%') AS res;
 
+-- Call sanitize_phone on every column containing SANITIZE_AS_PHONE in the comment
 SELECT sanitize_phone(res.table_name :: VARCHAR, res.column_name :: VARCHAR)
 FROM (SELECT
         c.table_name,
@@ -67,6 +86,7 @@ FROM (SELECT
                                                     AND c.table_schema = st.schemaname AND c.table_name = st.relname)
       WHERE pgd.description LIKE '%SANITIZE_AS_PHONE%') AS res;
 
+-- Cleanup the functions
 DROP FUNCTION sanitize_email(_t VARCHAR, _c VARCHAR );
 DROP FUNCTION sanitize_phone(_t VARCHAR, _c VARCHAR );
 DROP FUNCTION sanitize_value(_t VARCHAR, _c VARCHAR );
