fda48d74fefcd058333c85664f08dca1dc814503
Max Ryndin <rgmasim@gmail.com>
2018-11-30 00:04:06 +0300

    PLYO-652 Fix code review comment: use lowercase.

--- publisher/sql/sanitize.sql
+++ publisher/sql/sanitize.sql
@@ -1,92 +1,92 @@
 
--- We define sanitization functions first:
+-- we define sanitization functions first:
 -- - sanitize_email
 -- - sanitize_phone
 -- - sanitize_value
-CREATE OR REPLACE FUNCTION sanitize_email(_t VARCHAR, _c VARCHAR)
-  RETURNS VOID AS
+create or replace function sanitize_email(_t varchar, _c varchar)
+  returns void as
 $$
-DECLARE
-  affected NUMERIC;
-BEGIN
-  EXECUTE format('UPDATE "%s" SET "%s" = RIGHT(md5("%s"), 12) || SUBSTRING("%s", ''@.+$'');', _t, _c, _c, _c);
-  GET DIAGNOSTICS affected = ROW_COUNT;
-  RAISE NOTICE '%.% is sanitized: % rows affected', _t, _c, affected;
-END
+declare
+  affected numeric;
+begin
+  execute format('update "%s" set "%s" = right(md5("%s"), 12) || substring("%s", ''@.+$'');', _t, _c, _c, _c);
+  get diagnostics affected = row_count;
+  raise notice '%.% is sanitized: % rows affected', _t, _c, affected;
+end
 $$
-LANGUAGE plpgsql;
+language plpgsql;
 
-CREATE OR REPLACE FUNCTION sanitize_phone(_t VARCHAR, _c VARCHAR)
-  RETURNS VOID AS
+create or replace function sanitize_phone(_t varchar, _c varchar)
+  returns void as
 $$
-DECLARE
-  affected NUMERIC;
-BEGIN
-  EXECUTE format('UPDATE "%s" SET "%s" = ''93000000'' WHERE "%s" IS NOT NULL;', _t, _c, _c);
-  GET DIAGNOSTICS affected = ROW_COUNT;
-  RAISE NOTICE '%.% is sanitized: % rows affected', _t, _c, affected;
-END
+declare
+  affected numeric;
+begin
+  execute format('update "%s" set "%s" = ''93000000'' where "%s" is not null;', _t, _c, _c);
+  get diagnostics affected = row_count;
+  raise notice '%.% is sanitized: % rows affected', _t, _c, affected;
+end
 $$
-LANGUAGE plpgsql;
+language plpgsql;
 
-CREATE OR REPLACE FUNCTION sanitize_value(_t VARCHAR, _c VARCHAR)
-  RETURNS VOID AS
+create or replace function sanitize_value(_t varchar, _c varchar)
+  returns void as
 $$
-DECLARE
-  affected NUMERIC;
-BEGIN
-  EXECUTE format('UPDATE "%s" SET "%s" = RIGHT(md5("%s"), 12);', _t, _c, _c);
-  GET DIAGNOSTICS affected = ROW_COUNT;
-  RAISE NOTICE '%.% is sanitized: % rows affected', _t, _c, affected;
-END
+declare
+  affected numeric;
+begin
+  execute format('update "%s" set "%s" = right(md5("%s"), 12);', _t, _c, _c);
+  get diagnostics affected = row_count;
+  raise notice '%.% is sanitized: % rows affected', _t, _c, affected;
+end
 $$
-LANGUAGE plpgsql;
+language plpgsql;
 
--- This query is for demo purposes only. It shows all the columns with non-null comments.
--- The query is a base for filtering columns to sanitize.
-SELECT
+-- this query is for demo purposes only. it shows all the columns with non-null comments.
+-- the query is a base for filtering columns to sanitize.
+select
   c.table_name,
   c.column_name,
   pgd.description
-FROM pg_catalog.pg_statio_all_tables AS st
-  INNER JOIN pg_catalog.pg_description pgd ON (pgd.objoid = st.relid)
-  INNER JOIN information_schema.columns c ON (pgd.objsubid = c.ordinal_position
-                                              AND c.table_schema = st.schemaname AND c.table_name = st.relname);
+from pg_catalog.pg_statio_all_tables as st
+  inner join pg_catalog.pg_description pgd on (pgd.objoid = st.relid)
+  inner join information_schema.columns c on (pgd.objsubid = c.ordinal_position
+                                              and c.table_schema = st.schemaname and c.table_name = st.relname);
 
--- Call sanitize_email on every column containing SANITIZE_AS_EMAIL in the comment
-SELECT sanitize_email(res.table_name :: VARCHAR, res.column_name :: VARCHAR)
-FROM (SELECT
+-- call sanitize_email on every column containing sanitize_as_email in the comment
+select sanitize_email(res.table_name :: varchar, res.column_name :: varchar)
+from (select
         c.table_name,
         c.column_name
-      FROM pg_catalog.pg_statio_all_tables AS st
-        INNER JOIN pg_catalog.pg_description pgd ON (pgd.objoid = st.relid)
-        INNER JOIN information_schema.columns c ON (pgd.objsubid = c.ordinal_position
-                                                    AND c.table_schema = st.schemaname AND c.table_name = st.relname)
-      WHERE pgd.description LIKE '%SANITIZE_AS_EMAIL%') AS res;
+      from pg_catalog.pg_statio_all_tables as st
+        inner join pg_catalog.pg_description pgd on (pgd.objoid = st.relid)
+        inner join information_schema.columns c on (pgd.objsubid = c.ordinal_position
+                                                    and c.table_schema = st.schemaname and c.table_name = st.relname)
+      where pgd.description like '%SANITIZE_AS_EMAIL%') as res;
 
--- Call sanitize_value on every column containing SANITIZE_AS_VALUE in the comment
-SELECT sanitize_value(res.table_name :: VARCHAR, res.column_name :: VARCHAR)
-FROM (SELECT
+-- call sanitize_value on every column containing sanitize_as_value in the comment
+select sanitize_value(res.table_name :: varchar, res.column_name :: varchar)
+from (select
         c.table_name,
         c.column_name
-      FROM pg_catalog.pg_statio_all_tables AS st
-        INNER JOIN pg_catalog.pg_description pgd ON (pgd.objoid = st.relid)
-        INNER JOIN information_schema.columns c ON (pgd.objsubid = c.ordinal_position
-                                                    AND c.table_schema = st.schemaname AND c.table_name = st.relname)
-      WHERE pgd.description LIKE '%SANITIZE_AS_VALUE%') AS res;
+      from pg_catalog.pg_statio_all_tables as st
+        inner join pg_catalog.pg_description pgd on (pgd.objoid = st.relid)
+        inner join information_schema.columns c on (pgd.objsubid = c.ordinal_position
+                                                    and c.table_schema = st.schemaname and c.table_name = st.relname)
+      where pgd.description like '%SANITIZE_AS_VALUE%') as res;
 
--- Call sanitize_phone on every column containing SANITIZE_AS_PHONE in the comment
-SELECT sanitize_phone(res.table_name :: VARCHAR, res.column_name :: VARCHAR)
-FROM (SELECT
+-- call sanitize_phone on every column containing sanitize_as_phone in the comment
+select sanitize_phone(res.table_name :: varchar, res.column_name :: varchar)
+from (select
         c.table_name,
         c.column_name
-      FROM pg_catalog.pg_statio_all_tables AS st
-        INNER JOIN pg_catalog.pg_description pgd ON (pgd.objoid = st.relid)
-        INNER JOIN information_schema.columns c ON (pgd.objsubid = c.ordinal_position
-                                                    AND c.table_schema = st.schemaname AND c.table_name = st.relname)
-      WHERE pgd.description LIKE '%SANITIZE_AS_PHONE%') AS res;
+      from pg_catalog.pg_statio_all_tables as st
+        inner join pg_catalog.pg_description pgd on (pgd.objoid = st.relid)
+        inner join information_schema.columns c on (pgd.objsubid = c.ordinal_position
+                                                    and c.table_schema = st.schemaname and c.table_name = st.relname)
+      where pgd.description like '%SANITIZE_AS_PHONE%') as res;
 
--- Cleanup the functions
-DROP FUNCTION sanitize_email(_t VARCHAR, _c VARCHAR );
-DROP FUNCTION sanitize_phone(_t VARCHAR, _c VARCHAR );
-DROP FUNCTION sanitize_value(_t VARCHAR, _c VARCHAR );
+-- cleanup the functions
+drop function sanitize_email(_t varchar, _c varchar );
+drop function sanitize_phone(_t varchar, _c varchar );
+drop function sanitize_value(_t varchar, _c varchar );
