c7ab2b131b7d150aed6e1686a3201b1dabf9ebc2
Maxim Ryndin <masim05@users.noreply.github.com>
2018-12-19 17:29:24 +0300

    PLYO-652 DB sanitising (#29)
    * PLYO-652 Add a comment.
    
    * PLYO-652 Sanitize emails table.
    
    * PLYO-652 Code review fixes.
    
    * PLYO-652 Sanitise more data.
    
    * PLYO-652 Wait 1 min for DB to initialize.
    
    * PLYO-652 Fix the text.
    
    * PLYO-652 Use stored procedures.
    
    * PLYO-652 Sanitize data based on column comments.
    
    * PLYO-652 Add comments.
    
    * PLYO-652 Fix code review comment.
    
    * PLYO-652 Fix code review comment: use lowercase.
    
    * PLYO-652 Add more sanitizers: for nullable and jsonb.
    
    * PLYO-652 Correct comments.
    
    * PLYO-652 Use ilike.
    
    * PLYO-652 Mount sql folder.
    
    * PLYO-652 Copy sql file into contaiter.
    
    * PLYO-652 Use correct image.
    
    * PLYO-652 Several fixes for sanitizer
    
    - We using separate schema for sanitizing functions
    - Sanitizer container should have more env vars and behave as usual container ready for prod dump. It should have predefined users, schemas, extensions etc
    - Additional logging for publisher actions
    
    * PLYO-652 Removed unnecessary env var

---
+++ publisher/sql/sanitize.sql
@@ -0,0 +1,142 @@
+
+create schema sanitizing;
+
+-- we define sanitization functions first:
+-- - sanitize_email
+-- - sanitize_phone
+-- - sanitize_value
+-- - sanitize_nullable
+-- - sanitize_jsonb
+create or replace function sanitizing.sanitize_email(_t varchar, _c varchar)
+  returns void as
+$$
+declare
+  affected numeric;
+begin
+  execute format('update "%s" set "%s" = right(md5("%s"), 12) || substring("%s", ''@.+$'');', _t, _c, _c, _c);
+  get diagnostics affected = row_count;
+  raise notice '%.% is sanitized: % rows affected', _t, _c, affected;
+end
+$$
+language plpgsql;
+
+create or replace function sanitizing.sanitize_phone(_t varchar, _c varchar)
+  returns void as
+$$
+declare
+  affected numeric;
+begin
+  execute format('update "%s" set "%s" = ''93000000'' where "%s" is not null;', _t, _c, _c);
+  get diagnostics affected = row_count;
+  raise notice '%.% is sanitized: % rows affected', _t, _c, affected;
+end
+$$
+language plpgsql;
+
+create or replace function sanitizing.sanitize_value(_t varchar, _c varchar)
+  returns void as
+$$
+declare
+  affected numeric;
+begin
+  execute format('update "%s" set "%s" = right(md5("%s"), 12);', _t, _c, _c);
+  get diagnostics affected = row_count;
+  raise notice '%.% is sanitized: % rows affected', _t, _c, affected;
+end
+$$
+language plpgsql;
+
+create or replace function sanitizing.sanitize_nullable(_t varchar, _c varchar)
+  returns void as
+$$
+declare
+  affected numeric;
+begin
+  execute format('update "%s" set "%s" = NULL;', _t, _c, _c);
+  get diagnostics affected = row_count;
+  raise notice '%.% is sanitized: % rows affected', _t, _c, affected;
+end
+$$
+language plpgsql;
+
+create or replace function sanitizing.sanitize_jsonb(_t varchar, _c varchar)
+  returns void as
+$$
+declare
+  affected numeric;
+begin
+  execute format('update "%s" set "%s" = ''{}''::jsonb;', _t, _c, _c);
+  get diagnostics affected = row_count;
+  raise notice '%.% is sanitized: % rows affected', _t, _c, affected;
+end
+$$
+language plpgsql;
+
+-- this query is for demo purposes only. it shows all the columns with non-null comments.
+-- the query is a base for filtering columns to sanitize.
+select
+  c.table_name,
+  c.column_name,
+  pgd.description
+from pg_catalog.pg_statio_all_tables as st
+  inner join pg_catalog.pg_description pgd on (pgd.objoid = st.relid)
+  inner join information_schema.columns c on (pgd.objsubid = c.ordinal_position
+                                              and c.table_schema = st.schemaname and c.table_name = st.relname);
+
+-- call sanitize_email on every column containing SANITIZE_AS_EMAIL in the comment
+select sanitizing.sanitize_email(res.table_name :: varchar, res.column_name :: varchar)
+from (select
+        c.table_name,
+        c.column_name
+      from pg_catalog.pg_statio_all_tables as st
+        inner join pg_catalog.pg_description pgd on (pgd.objoid = st.relid)
+        inner join information_schema.columns c on (pgd.objsubid = c.ordinal_position
+                                                    and c.table_schema = st.schemaname and c.table_name = st.relname)
+      where pgd.description ilike '%SANITIZE_AS_EMAIL%') as res;
+
+-- call sanitize_value on every column containing SANITIZE_AS_VALUE in the comment
+select sanitizing.sanitize_value(res.table_name :: varchar, res.column_name :: varchar)
+from (select
+        c.table_name,
+        c.column_name
+      from pg_catalog.pg_statio_all_tables as st
+        inner join pg_catalog.pg_description pgd on (pgd.objoid = st.relid)
+        inner join information_schema.columns c on (pgd.objsubid = c.ordinal_position
+                                                    and c.table_schema = st.schemaname and c.table_name = st.relname)
+      where pgd.description ilike '%SANITIZE_AS_VALUE%') as res;
+
+-- call sanitize_phone on every column containing SANITIZE_AS_PHONE in the comment
+select sanitizing.sanitize_phone(res.table_name :: varchar, res.column_name :: varchar)
+from (select
+        c.table_name,
+        c.column_name
+      from pg_catalog.pg_statio_all_tables as st
+        inner join pg_catalog.pg_description pgd on (pgd.objoid = st.relid)
+        inner join information_schema.columns c on (pgd.objsubid = c.ordinal_position
+                                                    and c.table_schema = st.schemaname and c.table_name = st.relname)
+      where pgd.description ilike '%SANITIZE_AS_PHONE%') as res;
+
+-- call sanitize_nullable on every column containing SANITIZE_AS_NULLABLE in the comment
+select sanitizing.sanitize_nullable(res.table_name :: varchar, res.column_name :: varchar)
+from (select
+        c.table_name,
+        c.column_name
+      from pg_catalog.pg_statio_all_tables as st
+        inner join pg_catalog.pg_description pgd on (pgd.objoid = st.relid)
+        inner join information_schema.columns c on (pgd.objsubid = c.ordinal_position
+                                                    and c.table_schema = st.schemaname and c.table_name = st.relname)
+      where pgd.description ilike '%SANITIZE_AS_NULLABLE%') as res;
+
+-- call sanitize_jsonb on every column containing SANITIZE_AS_JSONB in the comment
+select sanitizing.sanitize_jsonb(res.table_name :: varchar, res.column_name :: varchar)
+from (select
+        c.table_name,
+        c.column_name
+      from pg_catalog.pg_statio_all_tables as st
+        inner join pg_catalog.pg_description pgd on (pgd.objoid = st.relid)
+        inner join information_schema.columns c on (pgd.objsubid = c.ordinal_position
+                                                    and c.table_schema = st.schemaname and c.table_name = st.relname)
+      where pgd.description ilike '%SANITIZE_AS_JSONB%') as res;
+
+-- cleanup the functions
+drop schema sanitizing cascade;
--- publisher/start.sh
+++ publisher/start.sh
@@ -1,46 +1,111 @@
 #!/usr/bin/env sh
+log () {
+  echo "[start.sh]> $@"
+}
+
+log "Logging in on registry.plyo.website"
 docker login -p=${DOCKERHUB_PASS} -u=${DOCKERHUB_LOGIN} registry.plyo.website
+
+log "Pulling fresh postgres image ${POSTGRES_IMAGE}"
 docker pull ${POSTGRES_IMAGE}
 
+log "Running publishing container in daemon mode, and start doing nothing"
 docker run --name publishing_db_container -d \
--v ${DUMPS_DIR}:/files \
--w /docker-entrypoint-initdb.d \
-${POSTGRES_IMAGE} tail -f /dev/null
+  -v ${DUMPS_DIR}:/files \
+  -w /docker-entrypoint-initdb.d \
+  ${POSTGRES_IMAGE} tail -f /dev/null
 
+log "Creating /dumps dir"
 docker exec publishing_db_container mkdir /dumps
 backup_date=`date +%Y-%m-%d-%H:00`
 backup_file=${backup_date}${DUMP_NAME_SUFFIX}
 backup_roles_file=${backup_file}_roles.out
 
-if [ $(docker exec publishing_db_container test -e "/files/$backup_file" && echo $?) ];
+log "Looking for ${backup_file} inside container"
+if [ $(docker exec publishing_db_container test -e "/files/${backup_file}" && echo $?) ];
 then
     # we preparing 2 docker images, one contains only needed roles - it's "empty" state of DB
     # another one contains data and can be used by developer and tools running tests on real data
+    log "Copying a file with dump of roles into publisher container"
     docker exec publishing_db_container cp /files/${backup_roles_file} /dumps/db_roles.out
 
     # restore.sh will be executed at the moment of container's start
+    log "Adding instruction for restoring roles into container startup script"
     cat <<-EORESTORE | (docker exec -i publishing_db_container sh -c "cat > restore.sh")
       psql -f /dumps/db_roles.out -U postgres
 EORESTORE
 
+    log "Stopping publishing container to commit roles-only image"
     docker stop publishing_db_container
     docker commit $(docker ps -a -f name=publishing_db_container -q) ${DESTINATION_DOCKER_IMAGE}-roles-only
+
+    log "Pushing a dump with roles to docker registry"
     docker push ${DESTINATION_DOCKER_IMAGE}-roles-only
 
+    log "Running publishing container again to put a dump with actual data"
     docker start publishing_db_container
-    docker exec publishing_db_container cp /files/${backup_file} /dumps/db.backup
 
+    # we need to sanitize sensitive data first
+    log "Running another postgres docker image for sanitizer"
+    docker run --name sanitizing_db_container -d --rm \
+      -v ${DUMPS_DIR}:/files \
+      -w /docker-entrypoint-initdb.d \
+      -e DB_NAME=${DB_NAME} \
+      -e APP_ROLE_NAME=${APP_ROLE_NAME} \
+      -e ADMIN_ROLE_NAME=${ADMIN_ROLE_NAME} \
+      -e SCHEMA_NAME=${SCHEMA_NAME} \
+      -e PRIVATE_SCHEMA_NAME=${PRIVATE_SCHEMA_NAME} \
+      ${POSTGRES_IMAGE}
+
+    db_initialized=0
+    log "Waiting 1 min for sanitizer to be initialized"
+    for i in `seq 1 60`;
+    do
+      docker exec sanitizing_db_container psql -U postgres -c "\t" &> /dev/null && db_initialized=1 && break
+      echo -n .
+      sleep 1
+    done
+
+    echo ""
+
+    if [ "$db_initialized" -eq 0 ]
+    then
+        log "Sanitizing DB was not initialized after 1 min, stopping..."
+        docker kill publishing_db_container
+        docker rm publishing_db_container
+        docker stop sanitizing_db_container
+        exit 0
+    fi
+
+    log "Restoring dumps to sanitizer container"
+    docker exec sanitizing_db_container psql -f /files/${backup_roles_file} -U postgres
+    docker exec sanitizing_db_container pg_restore /files/${backup_file} -U postgres -d ${DB_NAME}
+
+    log "Running sanitizing script inside the container"
+    docker cp sql/sanitize.sql sanitizing_db_container:/sanitize.sql
+    docker exec sanitizing_db_container psql -f /sanitize.sql -U postgres ${DB_NAME}
+
+    log "Making sanitized dump"
+    docker exec sanitizing_db_container pg_dump -Fc -U postgres -f /files/${backup_file}.sanitized -d ${DB_NAME} > /dev/null
+
+    log "Stopping sanitizer"
+    docker stop sanitizing_db_container
+
+    log "Copy sanitized dump into publisher and adding instruction to restore it into startup script"
+    docker exec publishing_db_container cp /files/${backup_file}.sanitized /dumps/db.backup
     cat <<-EORESTORE | (docker exec -i publishing_db_container sh -c "cat >> restore.sh")
       pg_restore /dumps/db.backup -U postgres -d ${DB_NAME}
 EORESTORE
 
-
+    log "Stopping publisher to commit a new image and push it to docker registry"
+    docker stop publishing_db_container
     docker commit $(docker ps -a -f name=publishing_db_container -q) ${DESTINATION_DOCKER_IMAGE}
     docker push ${DESTINATION_DOCKER_IMAGE}
 else
+    log "File ${backup_file} is not found, stopping publishing container"
     docker stop publishing_db_container
-    echo file ${backup_file} is not found
 fi
 
+log "Trying to kill publisher in case it was not stopped"
 docker kill publishing_db_container
 docker rm publishing_db_container
